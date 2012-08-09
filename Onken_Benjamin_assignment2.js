c7f666cef286d8b6eb16790b9409c27489a53465
Ben Onken <BenOnken@Bens-MacBook-Pro.local>
2012-08-08 22:44:40 -0500

    Made a few tweeks to first function, working on current bug in boolean function.

--- Onken_Benjamin_assignment2.js
+++ Onken_Benjamin_assignment2.js
@@ -3,42 +3,66 @@
 
 */
 
-var string,
-	number,
-	array;
+var greeting = "Good morning! Time to head out fishing!",
+	fishCaught = 12,
+	array = [1,2,3,4],
+	sackLunch = false,
+	militaryTime = 22;
 var output = function(out)
 {
-	console.log(output);
+	console.log(out);
 };
 var findANewSpot = function(newSpot)
 {
 	if(newSpot = true)
 	{
 		console.log("We decided that we would find a new fishing spot! So after driving for a bit we arrived at our new spot.");
-	};
-	else()
+	}
+	else
 	{
-		console.log("We have been catching a lot of fish at this spot, so we will continue to fish here!")
+		console.log("We will continue to fish here at our current spot!");
 	};
-
 };
 
-var lunch = function(time,broughtFood)
+var fishCaughtHere = function(fishCaught)
 {
+	if(fishCaught < 5)
+	{
+		var changeSpot = true;
+		findANewSpot(changeSpot);
+	}
+	else
+	{
+		var changeSpot = false;
+		findANewSpot(changeSpot);
+	};
+};
 
-}
+var lunch = function(timeByHour,broughtFood)
+{
+	if(timeByHour < 13 && timeByHour > 11)
+	{
+		lunchTime = true;
+	}
+	else
+	{
+		lunchTime = false;
+	};
+	return(lunchTime);
+};
 
 
+output(greeting);
+fishCaughtHere(fishCaught);
+lunch(militaryTime,sackLunch);
 
+var lunchTime;
 
-
-findANewSpot(true);
-lunch()
 if(lunchTime = true)
 {
 	output("Since its late enough, we decided to take a break and have some lunch!");
-};
-else()
+}
+else
 {
 	output("Its not quite late enough yet for lunch, so we can continue fishing!");
 };
