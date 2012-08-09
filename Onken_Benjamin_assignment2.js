7a3d0bd4ba75b3e6a9b9b509525900bb7b20253f
Ben Onken <BenOnken@Bens-MacBook-Pro.local>
2012-08-08 21:08:02 -0500

    Finished first procedure and began on boolean function

--- Onken_Benjamin_assignment2.js
+++ Onken_Benjamin_assignment2.js
@@ -3,3 +3,43 @@
 
 */
 
+var string,
+	number,
+	array;
+var output = function(out)
+{
+	console.log(output);
+};
+var findANewSpot = function(newSpot)
+{
+	if(newSpot = true)
+	{
+		console.log("We decided that we would find a new fishing spot! So after driving for a bit we arrived at our new spot.");
+	};
+	else()
+	{
+		console.log("We have been catching a lot of fish at this spot, so we will continue to fish here!")
+	};
+
+};
+
+var lunch = function(time,broughtFood)
+{
+
+}
+
+
+
+
+
+findANewSpot(true);
+lunch()
+if(lunchTime = true)
+{
+	output("Since its late enough, we decided to take a break and have some lunch!");
+};
+else()
+{
+	output("Its not quite late enough yet for lunch, so we can continue fishing!");
+};
+
